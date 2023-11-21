969ccc9f1d937aadcb96574d92e8970ea3633281
wzh4464 <32484940+wzh4464@users.noreply.github.com>
2023-11-21 12:05:22 +0800

    Update vectorize_and_save function to handle pyarrow ChunkedArray inputs and save batch results
    Signed-off-by: wzh4464 <32484940+wzh4464@users.noreply.github.com>

--- vecReuters.py
+++ vecReuters.py
@@ -3,7 +3,7 @@ File: /vecReuters.py
 Created Date: Thursday November 16th 2023
 Author: Zihan
 -----
-Last Modified: Monday, 20th November 2023 7:30:43 pm
+Last Modified: Monday, 20th November 2023 10:44:55 pm
 Modified By: the developer formerly known as Zihan at <wzh4464@gmail.com>
 -----
 HISTORY:
@@ -20,7 +20,7 @@ import numpy as np
 from functools import partial
 
 
-def vectorize_and_save(examples, tokenizer, model, resultPath, firstLayerName, lastLayerName):
+def vectorize_and_save(examples, tokenizer, model, resultPath) -> None:
     """
     Vectorizes the given examples using the provided tokenizer and saves the resulting vectors to disk.
 
@@ -35,36 +35,63 @@ def vectorize_and_save(examples, tokenizer, model, resultPath, firstLayerName, l
     Returns:
         A dictionary containing the vectors from the first layer and the last layer.
     """
+    import pyarrow  # 用于处理parquet文件
+    
+    if isinstance(examples['text'], pyarrow.lib.ChunkedArray):
+        examples['text'] = examples['text'].to_pylist()  # 或.to_numpy()
+
+    if isinstance(examples['new_id'], pyarrow.lib.ChunkedArray):
+        examples['new_id'] = examples['new_id'].to_pylist()  # 或.to_numpy()
+        
     # 对文本进行向量化
     inputs = tokenizer(
         examples['text'], padding='max_length', truncation=True, return_tensors="pt")
     # 把每个tensor单独转移到GPU上
     inputs = {k: v.cuda() for k, v in inputs.items()}
 
-    # print('inputs transformed to cuda')
-
-    with torch.no_grad():
-        # <class 'transformers.modeling_outputs.BaseModelOutputWithPoolingAndCrossAttentions'>
+    with torch.no_grad():  # 不计算梯度
+        # 使用模型进行预测
         outputs = model(**inputs)
-        # print('outputs generated')
 
-    # print('len(outputs.hidden_states):', len(outputs.hidden_states)) # tuple of 13 elements
+    first_layer = outputs.hidden_states[1].cpu().numpy()  # 第一层的输出
+    last_layer = outputs.hidden_states[-1].cpu().numpy()  # 最后一层的输出
 
-    first_layer = outputs.hidden_states[1].cpu().numpy()
-    last_layer = outputs.hidden_states[-1].cpu().numpy()
+    # reshape to (batch_size, seq_len * hidden_size)
+    first_layer = first_layer.reshape(first_layer.shape[0], -1)
+    last_layer = last_layer.reshape(last_layer.shape[0], -1)
+
+    print('first_layer.shape:', first_layer.shape)
+
+    # concat with new_id (new_id, layer_features)
+    new_id = np.array(examples['new_id']).reshape(-1, 1)
+
+    first_layer = np.concatenate((new_id, first_layer), axis=1)
+    last_layer = np.concatenate((new_id, last_layer), axis=1)
+
+    print('first_layer.shape:', first_layer.shape)
 
     # check path
     import os
     if not os.path.exists(resultPath):
         os.makedirs(resultPath)
+        print(f'created {resultPath}')
+    else:
+        print(f'{resultPath} already exists')
 
-    np.save(f'{resultPath}/{firstLayerName}', first_layer)
-    np.save(f'{resultPath}/{lastLayerName}', last_layer)
+    batch_index = examples['new_id'][0].split('"')[1]
+    # 保存每个批次的结果
+    batch_first_layer_name = f'{resultPath}/batch_{batch_index}_first_layer.npy'
+    batch_last_layer_name = f'{resultPath}/batch_{batch_index}_last_layer.npy'
+    np.save(batch_first_layer_name, first_layer)
+    np.save(batch_last_layer_name, last_layer)
 
-    return {'first_layer': first_layer, 'last_layer': last_layer}
+    print(f'batch {batch_index} saved at {batch_first_layer_name}')
+    print(f'batch {batch_index} saved at {batch_last_layer_name}')
+
+    # return {'first_layer': first_layer, 'last_layer': last_layer}
 
 
-def genVecMain():
+def genVecMain(batch_size=64, resultPath='/media/zihan/Ventoy/vecReutersResult'):
     # 检测是否有多个GPU
     if torch.cuda.device_count() > 1:
         print(f"发现 {torch.cuda.device_count()} GPUs.")
@@ -79,32 +106,49 @@ def genVecMain():
 
     dataset = load_dataset('reuters21578', 'ModHayes')
 
-    import time
-    curTime = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())
+    dataset.map(partial(vectorize_and_save, tokenizer=tokenizer, model=model,
+                        resultPath=resultPath), batched=True, batch_size=batch_size)
 
-    # 创建一个部分应用函数
-    vectorize_and_save_part = partial(
-        vectorize_and_save,
-        tokenizer=tokenizer,
-        model=model,
-        resultPath=f'result{curTime}',
-        firstLayerName='first_layer_features_gpu.npy',
-        lastLayerName='last_layer_features_gpu.npy',
-    )
+    # merged_first_layer, merged_last_layer = merge_batches(
+    #     resultPath, num_batches)
+    # 保存合并后的结果
+    # np.save(f'{resultPath}/merged_first_layer.npy', merged_first_layer)
+    # np.save(f'{resultPath}/merged_last_layer.npy', merged_last_layer)
 
-    # 使用部分应用函数和 map
-    dataset.map(vectorize_and_save_part, batched=True, batch_size=64)
 
 def showVecMain():
-    first_layer = np.load("../dataset/first_layer_features_gpu.npy")
-    last_layer = np.load("../dataset/last_layer_features_gpu.npy")
+    first_layer = np.load(
+        "result2023-11-20-13-05-44/first_layer_features_gpu.npy")
+    last_layer = np.load(
+        "result2023-11-20-13-05-44/last_layer_features_gpu.npy")
 
-    print('first_layer.shape:', first_layer.shape)  # (56, 512, 768)
+    # (56, 512, 768) batch_size, seq_len, hidden_size
+    print('first_layer.shape:', first_layer.shape)
     print('last_layer.shape:', last_layer.shape)
 
-    print('first_layer[0][0]:', first_layer[0][0])
+    # print('first_layer[0][0]:', first_layer[0][0])
+
+
+def merge_batches(resultPath, num_batches):
+    merged_first_layer = []
+    merged_last_layer = []
+
+    print('merging batches...')
+
+    for i in range(num_batches):
+        print(f'merging batch {i}...')
+        batch_first_layer = np.load(f'{resultPath}/batch_{i}_first_layer.npy')
+        batch_last_layer = np.load(f'{resultPath}/batch_{i}_last_layer.npy')
+
+        merged_first_layer.append(batch_first_layer)
+        merged_last_layer.append(batch_last_layer)
+
+    merged_first_layer = np.concatenate(merged_first_layer, axis=0)
+    merged_last_layer = np.concatenate(merged_last_layer, axis=0)
+
+    return merged_first_layer, merged_last_layer
 
 
 if __name__ == '__main__':
-    genVecMain()
+    genVecMain(batch_size=64)
     # showVecMain()
