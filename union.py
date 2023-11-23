226dc731bd03c755853a24c171b7fb7fd7ce9cae
wzh4464 <32484940+wzh4464@users.noreply.github.com>
2023-11-23 13:27:08 +0800

    Refactor file merging process to use multiprocessing

--- union.py
+++ union.py
@@ -3,7 +3,7 @@ File: /union.py
 Created Date: Wednesday November 22nd 2023
 Author: Zihan
 -----
-Last Modified: Thursday, 23rd November 2023 12:14:23 pm
+Last Modified: Thursday, 23rd November 2023 1:26:55 pm
 Modified By: the developer formerly known as Zihan at <wzh4464@gmail.com>
 -----
 HISTORY:
@@ -14,6 +14,7 @@ Date      		By   	Comments
 import numpy as np
 import os
 from tqdm import tqdm
+from multiprocessing import Pool
 
 def unionToOne(path):
     npzList = [os.path.join(root, file) for root, dirs, files in os.walk(
@@ -24,22 +25,27 @@ def unionToOne(path):
 
     print(f'{len(npzList)} npz files found at {path}')
 
-    # 顺序合并文件，并使用 tqdm 显示进度
-    for _ in tqdm(range(len(npzList) // 2), desc="Merging files"):
-        file1 = npzList.pop(0)
-        file2 = npzList.pop(0)
+    total_cpus = os.cpu_count()
+    reserved_cpus = 4  # 保留的 CPU 核心数
+    pool_size = max(1, total_cpus - reserved_cpus)
 
-        resultPath = os.path.join(path, f'{len(npzList)}.npz')
-        if concatResult := concatTwoFiles(file1, file2, resultPath):
-            npzList.append(concatResult)
+    while len(npzList) > 1:
+        with Pool(pool_size) as p:
+            results = []
+            for _ in tqdm(range(len(npzList) // 2), desc="Merging files"):
+                file1 = npzList.pop(0)
+                file2 = npzList.pop(0)
+                result_path = os.path.join(path, f'{len(npzList)}.npz')
+                async_result = p.apply_async(concatTwoFiles, args=(file1, file2, result_path))
+                results.append(async_result)
 
-    # 处理剩余文件
-    if npzList:
-        final_file = npzList[0]
-        os.rename(final_file, os.path.join(path, 'merged_result.npz'))
-        return True
-    else:
+            # 获取所有异步任务的结果
+            npzList.extend(async_result.get() for async_result in results)
+    if not npzList:
         return False
+    final_file = npzList[0]
+    os.rename(final_file, os.path.join(path, 'merged_result.npz'))
+    return True
 
 def concatTwoFiles(file1, file2, resultPath):
     print(f"Concatenating {file1} and {file2} to {resultPath}")
@@ -60,6 +66,7 @@ def concatHelper(file1, file2, resultPath):
     # 合并成功后删除原始文件
     os.remove(file1)
     os.remove(file2)
+    print(f"Concatenation of {file1} and {file2} to {resultPath} finished")
     return resultPath
 
 
